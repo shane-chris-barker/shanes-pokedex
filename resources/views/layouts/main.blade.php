c0d1e4d818b7d085c2ff8a6461e34e6b59af5744
Shane Chris Barker <geekychav@gmail.com>
2019-02-17 22:56:32 +0000

    [master] adding crsf token, adding fsavicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon">
+    <link rel="icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon">
     <title>@yield('title')</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/switcher.css')}}" rel="stylesheet" type="text/css">
-
-
 </head>
     <body>
         <div id="content" style="margin-bottom:5%; min-height:45em;">
